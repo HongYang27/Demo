de34ecb10c7b7dda7f079006b50f029d7ae9823c
HongYang27 <Kohhongyangnow@gmail.com>
2021-08-10 12:14:18 +0800

    hello

---
+++ demoVCS.py
@@ -0,0 +1,11 @@
+class Calculator:
+    def add(num1, num2):
+        total=num1+num2
+        return total
+
+    def difference(num1, num2):
+        minus=num1-num2
+        return minus
+
+Calculator.add(50, 40)
+Calculator.minus(50, 40)
